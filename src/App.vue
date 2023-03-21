110d96ef64d45ebebd2b474a2856bfceabd4e77f
Fernando Gonzalez <f.gonzalez@greening-group.com>
2023-03-21 12:28:12 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <!-- SIDEBAR -->
+    <SideBar/>
+  <v-main>
+    <!-- CARDS   -->
+  </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import SideBar from './components/SideBar.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    SideBar
+},
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-navigation-drawer
+        expand-on-hover
+        rail
+    >
+        <v-list>
+            <v-list-item
+                prepend-avatar="https://lh3.googleusercontent.com/a/AGNmyxaH0xSB_04VJoC5KLblOf52m5P8bf7YTIUsMQyNRw=s288"
+                title="Fernando Gonzalez"
+                subtitle="fgpaolini@gmailcom"
+            ></v-list-item>
+            </v-list>
+
+            <v-divider/>
+
+            <v-list density="compact" nav>
+                <v-list-item
+                    prepend-icon="mdi-image"
+                    title="Color Gallery"
+                    value="color"
+                />
+                <v-list-item
+                    prepend-icon="mdi-dialpad"
+                    title="Greyspace Gallery"
+                    value="color"
+                />
+                <v-list-item
+                    prepend-icon="mdi-message-text"
+                    title="Message"
+                    value="color"
+                />
+                <v-list-item
+                    prepend-icon="mdi-audio-video"
+                    title="Message"
+                    value="color"
+                />
+            </v-list>
+
+
+    </v-navigation-drawer>
+</template>
